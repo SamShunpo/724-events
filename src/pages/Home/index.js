ad0a42a12a6075ab080d30b2fdfbe6f377ffb270
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-09-03 21:41:22 +0200

    Last EventCard Label modification

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -125,7 +125,7 @@ const Page = () => {
           title={last?.title}
           date={new Date(last?.date)}
           small
-          label="boom"
+          label={last?.type}
         />
         ):(
           <p>Aucun élément disponible</p>
